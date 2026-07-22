4b9de23a0421607cc3936921298cda1760276bcc
William D.N <revege70@gmail.com>
2026-07-22 13:21:44 +0700

    update

--- service-antrean-fktp-add.bat
+++ service-antrean-fktp-add.bat
@@ -15,21 +15,29 @@ if not exist ".env" (
     goto :finish
 )
 
-if defined PHP_BIN if exist "%PHP_BIN%" goto :php_found
+set "PHP_OVERRIDE=%PHP_BIN%"
+set "PHP_BIN="
 
-for /f "delims=" %%I in ('where php 2^>nul') do set "PHP_BIN=%%I"
-if defined PHP_BIN if exist "%PHP_BIN%" goto :php_found
+if defined PHP_OVERRIDE call :try_php "%PHP_OVERRIDE%"
+if defined PHP_BIN goto :php_found
+
+call :try_php "C:\laragon\bin\php\php-8.3.16-Win32-vs16-x64\php.exe"
+if defined PHP_BIN goto :php_found
+
+for /f "delims=" %%I in ('where php 2^>nul') do call :try_php "%%I"
+if defined PHP_BIN goto :php_found
 
 for /d %%D in ("C:\laragon\bin\php\php-*") do (
-    if exist "%%~fD\php.exe" set "PHP_BIN=%%~fD\php.exe"
+    call :try_php "%%~fD\php.exe"
 )
-if defined PHP_BIN if exist "%PHP_BIN%" goto :php_found
+if defined PHP_BIN goto :php_found
 
-if exist "C:\xampp\php\php.exe" set "PHP_BIN=C:\xampp\php\php.exe"
-if defined PHP_BIN if exist "%PHP_BIN%" goto :php_found
+call :try_php "C:\xampp\php\php.exe"
+if defined PHP_BIN goto :php_found
 
-echo [ERROR] php.exe tidak ditemukan.
-echo Tambahkan folder PHP ke PATH atau set PHP_BIN ke lokasi php.exe.
+echo [ERROR] PHP dengan driver pdo_mysql tidak ditemukan.
+echo Aktifkan extension pdo_mysql pada php.ini atau set PHP_BIN
+echo ke lokasi php.exe yang memiliki driver MySQL.
 goto :finish
 
 :php_found
@@ -37,6 +45,8 @@ echo ==============================================
 echo SERVICE ADD ANTREAN FKTP
 echo Folder : %CD%
 echo PHP    : %PHP_BIN%
+"%PHP_BIN%" --ini
+"%PHP_BIN%" -r "echo 'PDO Drivers: '.implode(',', PDO::getAvailableDrivers()).PHP_EOL;"
 echo ==============================================
 echo.
 "%PHP_BIN%" artisan service:antrean-fktp-add
@@ -51,3 +61,11 @@ echo.
 echo Service berhenti. Tekan tombol apa saja untuk menutup.
 pause >nul
 endlocal
+exit /b
+
+:try_php
+if defined PHP_BIN exit /b
+if not exist "%~1" exit /b
+"%~1" -r "exit(in_array('mysql', PDO::getAvailableDrivers(), true) ? 0 : 1);" >nul 2>&1
+if not errorlevel 1 set "PHP_BIN=%~1"
+exit /b
